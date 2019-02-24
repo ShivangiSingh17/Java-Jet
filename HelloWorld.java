a994b3eb50d167abfb25247ea9574efd86ce21c6
Shivangi Singh <shivangisingh1702@gmail.com>
2019-02-24 12:14:42 +0530

    First simple program that prints Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/* This is a simple Java program.
+   FileName : "HelloWorld.java". */
+class HelloWorld
+{
+    // Program begins with a call to main().
+    // Prints "Hello World!!" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!!");
+    }
+}
