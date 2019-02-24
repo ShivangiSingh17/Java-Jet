890d4797a6fb01a5f908a83be9a8c5a613b2758f
Shivangi Singh <shivangisingh1702@gmail.com>
2019-02-24 14:41:20 +0530

    Example of Class in Java

---
+++ Class.java
@@ -0,0 +1,25 @@
+/*This is the way class is defined in Java:
+	class ClassName {
+    		//variables
+   		// methods
+	} 
+*/
+
+
+class switch {
+  // instance variable
+  private boolean isOn;
+  // method
+  public void turnOn() {
+    isOn = true;
+  }
+  // method
+  public void turnOff() {
+  	isOn = false;
+  }
+}
+
+
+//Here, class-"switch"
+//      instance variables-"isOn"
+//      methods-"turnOn , turnOff"
