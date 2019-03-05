4598df5340e712ffff7efd13890b8748b941105e
Shivangi Singh <shivangisingh1702@gmail.com>
2019-03-05 23:10:59 +0530

    Example of Array of Objects

---
+++ ObjectArray.java
@@ -0,0 +1,44 @@
+
+// Java program to illustrate creating an array of 
+// objects 
+
+class Student 
+{ 
+	public int roll_no; 
+	public String name; 
+	Student(int roll_no, String name) 
+	{ 
+		this.roll_no = roll_no; 
+		this.name = name; 
+	} 
+} 
+
+// Elements of array are objects of a class Student. 
+
+public class GFG 
+{ 
+	public static void main (String[] args) 
+	{ 
+		                                   // declares an Array of integers. 
+		Student[] arr; 
+
+		                              // allocating memory for 5 objects of type Student. 
+		arr = new Student[5]; 
+
+		                                    // initialize the first elements of the array 
+		arr[0] = new Student(1,"Atharv"); 
+
+	                                 	// initialize the second elements of the array 
+		arr[1] = new Student(2,"Vaibhav"); 
+
+		                                                                                                       // so on... 
+		arr[2] = new Student(3,"Gargi"); 
+		arr[3] = new Student(4,"Sunil"); 
+		arr[4] = new Student(5,"Shikhar"); 
+
+		                                     // accessing the elements of the specified array 
+		for (int i = 0; i < arr.length; i++) 
+			System.out.println("Element at " + i + " : " + 
+						arr[i].roll_no +" "+ arr[i].name); 
+	} 
+} 
