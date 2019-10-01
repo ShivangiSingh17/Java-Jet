e693713fdb446f206e47a87582d2451bfeaa301c
Piyush Mamidwar <mamidwar.piyush@gmail.com>
2019-10-02 01:03:48 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,1 +1,48 @@
-test
+// If we want to define dogs and cats and birds and we know all these
+// animals have names and ages we should create a superclass class.
+
+// SUPERCLASS
+class Animal {
+    private String name;
+    private int age;
+
+    public void identify() {
+        System.out.println("I am an animal!");
+    }
+
+    public void rename(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int newAge) {
+        if(newAge > 0)
+            age = newAge;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+// SUBCLASSES
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("Bark!");
+    }
+}
+
+class Cat extends Animal {
+    public void meow() {
+        System.out.println("Meow!");
+    }
+}
+
+class Bird extends Animal {
+    public void chirp() {
+        System.out.println("Chirp!");
+    }
+}
