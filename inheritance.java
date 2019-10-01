c49e51f23c12dd1707b3dd0ecbd1f7957fa735d3
Piyush Mamidwar <mamidwar.piyush@gmail.com>
2019-10-02 01:13:31 +0530

    Update inheritance.java

--- inheritance.java
+++ inheritance.java
@@ -1,3 +1,9 @@
+//Inheritance is an important pillar of OOP(Object Oriented Programming).
+//It is the mechanism in java by which one class is allow to inherit the features(fields and methods) of another class
+
+
+// Java program to demonstrate Inheritance 
+
 // If we want to define dogs and cats and birds and we know all these
 // animals have names and ages we should create a superclass class.
 
@@ -46,3 +52,19 @@ class Bird extends Animal {
         System.out.println("Chirp!");
     }
 }
+}
+public class Test  
+{ 
+    	// Driver code 
+    public static void main(String args[]) 
+	{ 
+		Dog dog = new Dog();
+        dog.setAge(5); //function of base class is called using an object of derived class.
+        System.out.println(dog.getAge());
+        dog.bark();
+        Cat cat = new Cat();
+        cat.identify();
+        cat.meow();
+        
+	} 
+} 
