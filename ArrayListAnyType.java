f7a1ad87fc606fcc7dcff735ef095b25ef371088
Fehim Mahfoud <fehim.mahfoud19@gmail.com>
2019-10-04 16:55:57 +0100

    Adding comments and more feature By getting from the list an specified type of data

--- ArrayListAnyType.java
+++ ArrayListAnyType.java
@@ -14,6 +14,18 @@ public void add(Object obj) {
 	arrayList.add(obj);
 }
 
+public ArrayList<Object> getAllInt(){
+		
+		ArrayList<Object> IntegerList = new ArrayList<Object>();
+		
+		for (int i = 0; i < this.arrayList.size() ; i++) {
+		    if (this.arrayList.get(i) instanceof Integer) {
+		    	IntegerList.add(this.arrayList.get(i));
+		    }
+		}
+		return IntegerList;
+	}
+
 public void remove(Object obj) {
 	arrayList.remove(obj);
 }
