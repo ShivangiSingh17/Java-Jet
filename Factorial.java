0798116590606f1b4c3638be099c8a5553c16e81
reshuraj57 <55954262+reshuraj57@users.noreply.github.com>
2019-10-08 15:48:57 +0530

    added code for finding factorial

---
+++ Factorial.java
@@ -0,0 +1,37 @@
+/*
+ * Factorial.java
+ * 
+ * Copyright 2019 Reshma-rajan <Reshma-rajan@RESHMA-PC>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
+
