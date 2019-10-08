4ee5db1c7ff2db810f274d8d45cdcb1204e42003
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-10-08 15:18:37 +0530

    Create factorialFinder.java

---
+++ factorialFinder.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       System.out.print("Please Input Your Number: ");
+       int input = scan.nextInt();
+       int output = factorial(input);
+       System.out.println("The factorial of " + input + " is: " + output);
+   }
+
+   public static int factorial(int input) {
+       int value = 1;
+       for (int i = 1; i <= input; i++) {
+           value = value * i;
+       }
+       return value;
+   }
+}
