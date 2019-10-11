29df954775ac91e9582fa2eafa9a4fdbeec76557
likhitha2497 <51660363+likhitha2497@users.noreply.github.com>
2019-10-11 02:48:05 -0400

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hello World!!");
+       System.out.println("welcome to java");
+       	System.out.println("java is object oriented programming");
     }
 }
