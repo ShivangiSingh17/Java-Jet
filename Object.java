cf6823c9d21e77b9f688ea2df00922f640c99f61
vishakha0505 <53859055+vishakha0505@users.noreply.github.com>
2019-10-13 16:01:34 +0530

    Update Object.java

--- Object.java
+++ Object.java
@@ -1,22 +1,9 @@
-// We created objects of "switch" class.
-// This program creates two objects s1 and s2 of class 'switch'.
 
+public class MyClass {
+  int x = 5;
 
-class switch {
-  boolean isOn;
-
-  void turnOn() {
-    isOn = true;
-  }
-
-  void turnOff() {
-   isOn = false;
-  }
-}
-
-class ClassObjectsExample {
-public static void main(String[] args) {
-   switch s1 = new switch(); // create s1 object of switch class
-   switch s2 = new switch(); // create s2 object of switch class
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
   }
 }
