d396440986e89b667801b18d43c0cdc339818ff4
khushbu1998 <33743028+khushbu1998@users.noreply.github.com>
2019-10-13 10:53:07 +0530

    adding program to print no divisble by 7

---
+++ print_divisible_by_7.java
@@ -0,0 +1,10 @@
+public class print_divisible_by_7 {
+
+	public static void main(String[] args) {
+		for(int i=100;i>1;i--) {
+        	if(i%7==0) {
+        		System.out.println(i);
+        	}
+        }
+    }
+}
