1927e878a01f36363a0c17f6527bb323e5471f50
gautam2907 <gautambhatiani.1999@gmail.com>
2019-10-15 15:35:44 +0530

    Created Methods.java

---
+++ Methods.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+class Methods{
+	
+	//Method Defination
+	private static void myMethod(){
+		System.out.println("Method is called");
+	}
+
+
+	public static void main(String[] args){
+		//Method call
+		myMethod();
+
+		System.out.println("The method is executed successfully");
+
+
+	}
+}
