4d3982e80125d3e53e2d694d930e5db20a7fe148
karan singh <56734381+nooob-coder@users.noreply.github.com>
2019-10-19 02:07:01 +0530

    created the desired pattern

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pattern
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter n:\n");
+		int n = sc.nextInt();
+
+		for(int i=0; i<n; i++)
+		{
+			System.out.println("*\n");
+		}
+	}
+}
