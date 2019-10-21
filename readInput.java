d3613fd4972119e26090174a78dc8c391e3a23b2
RyuZuko <56693487+RyuZuko@users.noreply.github.com>
2019-10-21 10:12:10 +0800

    Create ReadInput.java
    example on how to read input in java

---
+++ readInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class ReadInput{
+public static void main(string[]args){
+Scanner read = new Scanner(System.in);
+
+String name;
+
+System.out.println("Please enter your name: ")
+name = read.next();
+
+System.out.println("Your name is: "+name);
+}
+}
